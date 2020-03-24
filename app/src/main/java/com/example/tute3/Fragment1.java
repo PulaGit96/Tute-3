673a16bed3b8a0b86d38c782f3a66ee2f4635cfd
PulaGit96 <pulasthisandaruwan1@gmail.com>
2020-03-25 00:11:41 +0530

    First commit

---
+++ app/src/main/java/com/example/tute3/Fragment1.java
@@ -0,0 +1,18 @@
+package com.example.tute3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_fragment1, container, false);
+    }
+}
